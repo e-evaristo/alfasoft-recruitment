2f12570e1541e5540cb311099e921719830df13e
e-evaristo <emanoelevaristo@hotmail.com>
2022-04-16 10:39:08 -0300

    Add Migrations, Seeders, Laravel Breeze

---
+++ app/Models/Contact.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Contact extends Model
+{
+    use SoftDeletes;
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'contact', 'email',
+    ];
+}
---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'contact' => $this->faker->unique()->randomNumber($nbDigits = 9, $strict = true),
+            'email' => $this->faker->unique()->safeEmail,
+        ];
+    }
+}
